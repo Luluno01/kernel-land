fcebee3e29c5d8d781e9c6e2af7ca774bd77a09a
Untitled <gnu.imm@outlook.com>
2021-06-27 18:01:05 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -6,6 +6,6 @@ alias dkconf='dconf && kconf'
 alias uconf='node ../update-config.js'
 alias oconf='make CC=$CC olddefconfig'
 alias mkconf='dkconf && uconf && oconf && uconf && $CC -v 2>CC'
-alias paddr='git am ../linux.git/0001-selinux-use-kernel-linux-socket.h-for-genheaders-and.patch'
+alias paddr='git am ../patches/0001-selinux-use-kernel-linux-socket.h-for-genheaders-and.patch'
 alias mk='make CC=$CC'
 
