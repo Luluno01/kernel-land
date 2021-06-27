f3dba2deb5d850fbf3a148d08920aa166b245b69
Untitled <gnu.imm@outlook.com>
2021-06-27 21:11:39 +0800

    update scripts

--- env.sh
+++ env.sh
@@ -9,3 +9,46 @@ alias mkconf='dkconf && uconf && oconf && uconf && $CC -v 2>CC'
 alias paddr='git am ../patches/0001-selinux-use-kernel-linux-socket.h-for-genheaders-and.patch'
 alias mk='make CC=$CC'
 
+lunch() {
+  if [ "$1" = "" ]; then
+    echo Please specify a kernel version, e.g., v4.17
+    echo Usage:
+    echo "  lunch <version> [image=buster.img] [key=buster.id_rsa]"
+    return 1
+  fi
+  export KERNEL=`pwd`/$1
+  export IMAGE=${2:-buster.img}
+  export KEY=${3:-buster.id_rsa}
+  echo KERNEL=$KERNEL
+  echo IMAGE=$IMAGE
+  echo KEY=$KEY
+}
+
+start() {
+  export PORT=${1:-10022}
+  echo "Guest port 22 => host port $PORT"
+  echo Starting VM via \`screen\`
+  screen -d -m qemu-system-x86_64 \
+    -m 2G \
+    -smp 2 \
+    -kernel $KERNEL/arch/x86/boot/bzImage \
+    -append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0" \
+    -drive file=$IMAGE,format=raw \
+    -device e1000,netdev=net0 \
+    -netdev user,id=net0,hostfwd=tcp:127.0.0.1:$PORT-:22 \
+    -enable-kvm \
+    -nographic \
+    -pidfile vm.pid \
+    -snapshot
+}
+
+conn() {
+  if [ "$KEY" = "" ]; then
+    echo Warning: environment variable KEY not set
+  fi
+  if [ "$PORT" = "" ]; then
+    echo Warning: environment variable PORT not set
+  fi
+  ssh -i $KEY -p $PORT -o "StrictHostKeyChecking no" root@localhost
+}
+alias con='conn'
